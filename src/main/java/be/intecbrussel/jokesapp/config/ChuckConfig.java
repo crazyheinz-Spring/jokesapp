39ae41cfde547c804c301ceca35568871bdef381
crazyheinz <hannesdb@hotmail.com>
2019-11-27 10:04:53 +0100

    1st commit

---
+++ src/main/java/be/intecbrussel/jokesapp/config/ChuckConfig.java
@@ -0,0 +1,16 @@
+package be.intecbrussel.jokesapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+public class ChuckConfig {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+
+        return new ChuckNorrisQuotes();
+    }
+}
